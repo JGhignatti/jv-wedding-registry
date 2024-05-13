5862c2b6ed3a2767798bc842d808282d2d25c2b2
JGhignatti <ghignatti.joao@gmail.com>
2024-05-12 22:16:16 -0300

    feat: rewrite nav component to use popover hamburger menu

--- src/lib/components/Nav/Nav.svelte
+++ src/lib/components/Nav/Nav.svelte
@@ -1,33 +1,65 @@
+<script lang="ts">
+  let open = false;
+</script>
+
 {#snippet logo()}
-  <div class="w-fit">Bride & Groom</div>
+  <div class="flex items-center justify-center p-6 border-2 border-zinc-400">Noiva & Noivo</div>
 {/snippet}
 
-{#snippet navItem({ text, path })}
-  <a href={path}>{ text }</a>
-{/snippet}
+<div class="container mx-auto p-4 space-y-6">
+  <ul class="flex items-center gap-4">
+    <li class="mr-auto">
+      <a href="/" class="hidden md:block">Início</a>
 
-<div class="container mx-auto p-4">
-  <div class="flex justify-center items-center md:hidden">
-    {@render logo()}
-  </div>
-
-  <ul class="flex justify-between items-center gap-4">
-    <li>
-      {@render navItem({ text: 'Início', path: '/' })}
-    </li>
-    <li>
-      {@render navItem({ text: 'Informações', path: '/about' })}
-    </li>
-    <li class="hidden md:block mx-auto">
-      <a href="/">
+      <a href="/" class="block md:hidden">
         {@render logo()}
       </a>
     </li>
+
+    <div class="hidden md:contents">
+      <li>
+        <a href="/store">Loja</a>
+      </li>
+      <li>
+        <a href="/info">Informações</a>
+      </li>
+    </div>
+
     <li>
-      {@render navItem({ text: 'Loja', path: '/store' })}
+      <a href="/cart">Carrinho</a>
+    </li>
+
+    <li class="block md:hidden">
+      <button onclick={() => (open = true)}>M</button>
+    </li>
+  </ul>
+
+  <div class="hidden md:block">
+    {@render logo()}
+  </div>
+</div>
+
+<div
+  class="fixed w-full h-full inset-y-0 bg-zinc-200 flex-col flex transition-all md:hidden"
+  class:left-full={!open}
+  class:left-0={open}
+>
+  <button class="self-end p-4" onclick={() => (open = false)}>X</button>
+
+  {@render logo()}
+
+  <ul class="p-8">
+    <li>
+      <a href="/">Início</a>
     </li>
     <li>
-      {@render navItem({ text: 'Carrinho', path: '/cart' })}
+      <a href="/store">Loja</a>
+    </li>
+    <li>
+      <a href="/info">Informações</a>
+    </li>
+    <li>
+      <a href="/cart">Carrinho</a>
     </li>
   </ul>
 </div>
